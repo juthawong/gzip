ee7d411d98a0b4a26c924e75e757741093e6d266
Dominic Barnes <dominic@dbarnes.info>
2016-09-13 23:49:19 -0700

    update mako-utils, debug: include size difference before/after

--- index.js
+++ index.js
@@ -9,15 +9,21 @@ module.exports = function (extensions) {
 
   return function (mako) {
     mako.prewrite(extensions, function gzip (file, build, done) {
-      debug('compressing %s', utils.relative(file.path))
+      let relative = utils.relative(file.path)
+      let before = utils.size(file.contents, true)
+      debug('compressing %s', relative)
 
       zlib.gzip(file.contents, function (err, buf) {
         if (err) return done(err)
+
+        // TODO: not adding this file as a dependency, since really isn't one.
         build.tree.addFile({
           path: `${file.path}.gz`,
           contents: buf
         })
-        // TODO: not adding this file as a dependency, since really isn't one.
+
+        let after = utils.size(buf, true)
+        debug('compressed %s %s', relative, utils.sizeDiff(before, after))
         done()
       })
     })
